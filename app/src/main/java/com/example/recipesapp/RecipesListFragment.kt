3c4c84975ad04ba362dacb441b9a651f15ca28e8
Dmitry Litun <dlitun@ya.ru>
2026-03-16 19:23:17 +0300

    RA-10: recipes-bundle-navigation

--- app/src/main/java/com/example/recipesapp/RecipesListFragment.kt
+++ app/src/main/java/com/example/recipesapp/RecipesListFragment.kt
@@ -9,6 +9,16 @@ import com.example.recipesapp.databinding.FragmentRecipesListBinding
 
 class RecipesListFragment : Fragment() {
 
+    companion object {
+        const val ARG_CATEGORY_ID = "ARG_CATEGORY_ID"
+        const val ARG_CATEGORY_NAME = "ARG_CATEGORY_NAME"
+        const val ARG_CATEGORY_IMAGE_URL = "ARG_CATEGORY_IMAGE_URL"
+    }
+
+    private var categoryId: Int? = null
+    private var categoryName: String? = null
+    private var categoryImageUrl: String? = null
+
     private var _binding: FragmentRecipesListBinding? = null
     private val binding get() = _binding ?: error("FragmentRecipesListBinding is null")
 
@@ -19,6 +29,13 @@ class RecipesListFragment : Fragment() {
     ): View {
         _binding = FragmentRecipesListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        categoryId = arguments?.getInt(ARG_CATEGORY_ID)
+        categoryName = arguments?.getString(ARG_CATEGORY_NAME)
+        categoryImageUrl = arguments?.getString(ARG_CATEGORY_IMAGE_URL)
     }
 
     override fun onDestroyView() {
