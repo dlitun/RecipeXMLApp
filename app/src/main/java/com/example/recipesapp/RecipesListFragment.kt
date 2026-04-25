23441b11b1c0de30f48b939c98693aeb56146c80
Dmitry Litun <dlitun@ya.ru>
2026-04-25 12:51:50 +0300

    RA-11: recipes list screen

--- app/src/main/java/com/example/recipesapp/RecipesListFragment.kt
+++ app/src/main/java/com/example/recipesapp/RecipesListFragment.kt
@@ -1,10 +1,14 @@
 package com.example.recipesapp
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.recipesapp.databinding.FragmentRecipesListBinding
 
 class RecipesListFragment : Fragment() {
@@ -36,6 +40,45 @@ class RecipesListFragment : Fragment() {
         categoryId = arguments?.getInt(ARG_CATEGORY_ID)
         categoryName = arguments?.getString(ARG_CATEGORY_NAME)
         categoryImageUrl = arguments?.getString(ARG_CATEGORY_IMAGE_URL)
+
+        initHeader()
+        initRecycler()
+    }
+
+    private fun initHeader() {
+        binding.tvHeaderTitle.text = categoryName ?: getString(R.string.title_recipes)
+
+        val imageName = categoryImageUrl ?: return
+        try {
+            requireContext().assets.open(imageName).use { inputStream ->
+                val drawable = Drawable.createFromStream(inputStream, imageName)
+                binding.ivHeader.setImageDrawable(drawable)
+            }
+        } catch (e: Exception) {
+            Log.e("RecipesListFragment", "Error loading header image: $imageName", e)
+        }
+    }
+
+    private fun initRecycler() {
+        val recipes = STUB.getRecipesByCategoryId(categoryId ?: -1)
+        val adapter = RecipesListAdapter(recipes)
+
+        adapter.setOnItemClickListener(object : RecipesListAdapter.OnItemClickListener {
+            override fun onItemClick(recipeId: Int) {
+                openRecipeByRecipeId(recipeId)
+            }
+        })
+
+        binding.rvRecipes.adapter = adapter
+    }
+
+    private fun openRecipeByRecipeId(recipeId: Int) {
+        Log.d("RecipesListFragment", "Open recipeId=$recipeId")
+        parentFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<RecipeFragment>(R.id.mainContainer)
+            addToBackStack(null)
+        }
     }
 
     override fun onDestroyView() {
